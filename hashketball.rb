6cf5ba2980ee500d3d8dead7bee5e9818a6726e6
William Chabla <w_chabla@hotmail.com>
2018-06-23 07:30:50 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -66,7 +66,7 @@ def game_hash
           points: 10,
           rebounds: 1,
           assists: 1,
-          steals: 1,
+          steals: 2,
           blocks: 7,
           slam_dunks: 2
         },
