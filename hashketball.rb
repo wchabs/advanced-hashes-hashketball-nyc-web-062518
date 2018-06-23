7f32abbdf8619dca69548cf035d5801e3b4e5a9c
William Chabla <w_chabla@hotmail.com>
2018-06-23 06:22:17 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -171,7 +171,7 @@ def team_colors(team_name)
   
   all_data.each do |location, game_data|
     game_data.each do |data, details|
-      puts data.keys
+      puts details.keys
       if details == team_name
         puts details
         puts data
